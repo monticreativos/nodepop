71c8e7b700a6da36e40489fb0afe7ca50b5c70d9
David Montalba <davidmontalba1985@gmail.com>
2022-02-03 21:03:21 +0100

    app modificando

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const Server = require('./models/server');
 
 const server = new Server();
 
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
 
 server.listen();
